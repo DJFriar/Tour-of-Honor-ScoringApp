5c97266e23b144a970588a1c9fd222e79dae4834
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2025-07-03 23:46:52 -0500

    removed commented out code

--- app/navigation/SubmissionNavigator.js
+++ app/navigation/SubmissionNavigator.js
@@ -3,7 +3,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import SubmissionListScreen from '../screens/SubmissionListScreen';
 import SubmissionDetailScreen from '../screens/SubmissionDetailScreen';
-// import SubmissionSubmitScreen from '../screens/SubmissionSubmitScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,9 +15,6 @@ const SubmissionNavigator = () => (
       <Stack.Screen name="SubmissionList" component={SubmissionListScreen} />
       <Stack.Screen name="SubmissionDetailScreen" component={SubmissionDetailScreen} />
     </Stack.Group>
-    {/* <Stack.Group screenOptions={{ presentation: 'modal' }}>
-      <Stack.Screen name="SubmissionSubmitScreen" component={SubmissionSubmitScreen} options={({route}) => ({ title: 'Submit ' + route.params.code })} />
-    </Stack.Group> */}
   </Stack.Navigator>
 )
 
